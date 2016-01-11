1b4812352f5936fa7b248aafc11f795955454f94
Arnout Engelen <arnouten@bzzt.net>
2016-01-11 09:13:24 +0100

    Simplify counting inclusive time in call stack
    Now over-estimates it by a bit, not sure yet why.

--- ZshXtraceToCallgrind.ml
+++ ZshXtraceToCallgrind.ml
@@ -114,17 +114,21 @@ let rec pop_calls n stack calls last_micros =
       let updated_head = {next_head with micros_inclusive = next_head.micros_inclusive + popped.micros_inclusive + last_micros  } in
       pop_calls (n-1) (updated_head :: tail) (call :: calls) 0;;
 
+let add_micros_to_top stack micros =
+  let current_frame = List.hd stack in
+  let updated_frame = { current_frame with micros_inclusive = current_frame.micros_inclusive +  micros } in
+  updated_frame :: (List.tl stack)
+
 let update_stack from next stack =
   let newstack = add_stackframes from next stack in
   let last_micros = next.timestamp - from.timestamp in
+  let newstack_with_micros = add_micros_to_top newstack last_micros in
   if (from.depth = next.depth && from.depth = List.length stack) then
-    let current_frame = List.hd newstack in
-    let updated_frame = { current_frame with micros_inclusive = current_frame.micros_inclusive + last_micros } in
-    (updated_frame :: (List.tl newstack), [])
+    (newstack_with_micros, [])
   else
   let finished_calls = List.length newstack - next.depth in
   if finished_calls > 0 then pop_calls finished_calls newstack [] last_micros
-  else (newstack, []);;
+  else (newstack_with_micros, []);;
 
 let rec process_lines lastline profile_so_far =
   try
